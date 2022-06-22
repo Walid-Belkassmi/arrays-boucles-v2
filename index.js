db0ed42b465d5dfaa8d019183e80e5b12508d99b
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-06-22 15:30:12 +0200

    ex 6

---
+++ index.js
@@ -0,0 +1,61 @@
+// 1 - Nombre pairs
+// let total = 0
+// for(let i = 11; i < 47; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//         total += i
+//     }
+// }
+// console.log(total)
+
+// 2 - Nombre impairs
+// let total = 0
+// for(let i = 109; i < 588; i++){
+//     if(i % 2 === 1){
+//         console.log(i)
+//         total += i
+//     }
+// }
+// console.log(total)
+
+// 3 - Puissance et racine carrée
+// const numbers = [1, 2, 3, 4, 5]
+// const squares = []
+// const roots = []
+
+// for(let i = 0; i < numbers.length; i++){
+//     squares[i] = numbers[i] ** 2
+// }
+// console.log(squares)
+
+// for(let j = 0; j < numbers.length; j++){
+//     roots[j] = Math.sqrt(squares[j])
+// }
+// console.log(roots)
+
+// 4 - Table de multiplication
+// let res = 0
+// for(let i = 0; i <= 10; i++){
+//     res = 3 * i
+//     console.log(`3 x ${i} = ${res}`)
+// }
+
+// 5 - Plusieurs tables de multiplication
+// let res = 0
+// for(let i = 0; i <= 10; i++){
+//     for(let j = 0; j <= 10; j++){
+//         res = i * j
+//         console.log(`${i} x ${j} = ${res}`)
+//     }
+//     console.log("")
+// }
+
+// 6 - Factorielle v1
+// const number = 7
+// let fact = 1
+
+// for(let i = number; i >= 1; i--){
+//     fact = i * fact
+//     console.log(fact)
+// }
+
